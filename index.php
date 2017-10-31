a947c1d25a635d815c79ae4a768a50eed1f8e4ce
alex <agubarev@spotoption.com>
2017-10-31 10:45:16 +0200

    added comments

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-define('NUMBER_TIMES_RELOAD_IP', 3);
+define('NUMBER_TIMES_RELOAD_IP', 50);
 define('PARSE_URL', 'https://www.amazon.com/AmazonBasics-360-Piece-Clear-Plastic-Cutlery/dp/B010RLC7P2/ref=sr_1_1?s=kitchen&srs=10112675011&ie=UTF8&qid=1509179091&sr=1-1');
 define('TOR_PASSWORD', 'my_password');
-define('SEARCH_BLOCKED_REGEXP', 'robot');
+define('SEARCH_BLOCKED_REGEXP', 'AmazonBasics 360-Piece Clear Plastic Cutlery Set');
 
 // Autoloading using composer
 require 'vendor/autoload.php';
@@ -15,9 +15,14 @@ function get($url,$proxy) {
     curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; ru; rv:1.9.0.1) Gecko/2008070208');
     curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
     curl_setopt($ch, CURLOPT_PROXY, "$proxy");
-    $ss = curl_exec($ch);
+    $output = curl_exec($ch);
+    $info = curl_getinfo($ch);
     curl_close($ch);
-    return $ss;
+
+    return [
+        'output' => $output,
+        'info' => $info,
+    ];
 }
 
 function reloadIP()
@@ -46,12 +51,27 @@ function reloadIP()
     return $res[0]['code'].': '.$res[0]['message'];
 }
 
+
+$time_start = microtime(1);
+
 $prox = 'localhost:9050';
 $j = 0;
-for ($i=1; $i<=5;$i++) {
+for ($i=1; $i<=100;$i++) {
     $a = get(PARSE_URL,$prox);
-    preg_match('/' . SEARCH_BLOCKED_REGEXP .'/', $a, $matches) == 1 ? $j++ : null;
+
+    $info = $a['info'];
+echo $response = $a['output'];
+echo $info['http_code'];
+    if ($info['http_code']!==200 || preg_match('/' . SEARCH_BLOCKED_REGEXP .'/', $response, $matches) !== 1)
+    {
+        $j++;
+    }
+var_export($matches);
 }
+
+$time_end = microtime(1);
+
+echo " * Finished in ".round($time_end-$time_start,2)."s";
 
 if ($j >= NUMBER_TIMES_RELOAD_IP) {
     $res = reloadIP();
